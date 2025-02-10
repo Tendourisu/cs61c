531e070f6b32f1ed461010b01f82580e8854976c
al1s <hd-zhu23@mails.tsinghua.edu.cn>
2025-02-10 17:17:10 +0800

    proj2 completed

--- sp22-proj2-starter/src/read_matrix.s
+++ sp22-proj2-starter/src/read_matrix.s
@@ -27,16 +27,93 @@
 read_matrix:
 
 	# Prologue
+	ebreak
+	addi sp, sp, -24
+	sw ra, 0(sp)
+    sw s0, 4(sp)
+	sw s1, 8(sp)
+    sw s2, 12(sp)
+	sw s3, 16(sp) # descriptor if the file
+	sw s4, 20(sp) # pointer of the allocated heap memory
 
+	mv s0 a0
+	mv s1 a1
+	mv s2 a2
 
+	mv a0 s0
+	li a1 0
+	jal ra fopen
+	li t0 -1
+	beq a0 t0 exit27
+   
+    mv s3 a0 # record the descriptor of the file
 
+	mv a0 s3
+	mv a1 s1
+	li a2 4
+	jal ra fread
+	li t0 4
+	bne a0 t0 exit29
 
+	mv a0 s3
+    mv a1 s2
+	li a2 4 
+	jal ra fread
+	li t0 4
+	bne a0 t0 exit29
 
+	lw t0 0(s1)
+	lw t1 0(s2)
+	mul t2 t0 t1
+	slli t2 t2 2
+	mv a0 t2
+	jal ra malloc
+	beq a0 x0 exit26
 
+	mv s4 a0
+	mv a0 s3
+	mv a1 s4
+	lw t0 0(s1)
+	lw t1 0(s2)
+	mul t2 t0 t1
+	slli t2 t2 2
+	mv a2 t2
+	jal ra fread
+	lw t0 0(s1)	
+	lw t1 0(s2)
+	mul t2 t0 t1
+	slli t2 t2 2
+	bne a0 t2 exit29
 
-
-
+	mv a0 s3
+	jal ra fclose
+	bne a0 x0 exit28
+	
 	# Epilogue
-
+	mv a0 s4
+	lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+	lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+	addi sp, sp, 24
 
 	ret
+
+exit26:
+	li a0 17
+	li a1 26
+	ecall
+exit27:
+	li a0 17
+	li a1 27
+	ecall
+exit28:
+	li a0 17
+	li a1 28
+	ecall
+exit29:
+	li a0 17
+	li a1 29
+	ecall
